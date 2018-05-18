29f19241e23078ebe29ad48d79ea8c6861b4c4e7
George Roger <georger@xmos.com>
2018-05-18 14:13:32 +0100

    Fixed bug where dl would follow symlinks when emptying trash
    This could cause the unintended deletion files.
    Replaced the custom delete function with a subprocess call to
      "rm -rfv 'trash_path'"

--- empty.py
+++ empty.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python3
 import trash
-
-
-class Deleter:
-    def __init__(self, logger):
-        self.logger = logger
-        self.file_count = 0
-        self.directory_count = 0
-
-    def delete_path(self, path):
-        for item in path.glob('*'):
-            self.delete_path(item)
-
-        if path.is_dir():
-            self.logger.print('rmdir {}'.format(path), 2)
-            path.rmdir()
-            self.directory_count += 1
-        else:
-            self.logger.print('rm {}'.format(path), 2)
-            path.unlink()
-            self.file_count += 1
+import subprocess
 
 
 def run(logger):
@@ -28,11 +9,7 @@ def run(logger):
     logger.print('Emptying {}'.format(trash_path), 1)
     logger.print('', 2)
 
-    deleter = Deleter(logger)
-    deleter.delete_path(trash_path)
-
-    logger.print('', 2)
-    logger.print('Removed {} items'.format(
-        deleter.file_count + deleter.directory_count), 1)
-    logger.print('  {} files'.format(deleter.file_count), 1)
-    logger.print('  {} directories'.format(deleter.directory_count), 1)
+    if trash_path:
+        cmd = ['rm', '-rfv', trash_path]
+        print('{}'.format(' '.join(map(str, cmd))))
+        subprocess.run(cmd)
